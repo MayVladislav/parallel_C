29331426e5d7a9ae5c66ff576b6c99ae9be1f18a
MayVladislav <56929850+MayVladislav@users.noreply.github.com>
2023-02-17 20:11:53 +0300

    Add files via upload

---
+++ LR2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] matrix2 = new int[3, 3] { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
+            int[,] resultMatrix = MatrixSum(matrix1, matrix2, (x, y) => x + y);
+
+            Console.WriteLine("Result matrix:");
+            for (int i = 0; i < resultMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < resultMatrix.GetLength(1); j++)
+                {
+                    Console.Write(resultMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+        static int[,] MatrixSum(int[,] matrix1, int[,] matrix2, Func<int, int, int> sumFunction)
+        {
+            int rows = matrix1.GetLength(0);
+            int columns = matrix1.GetLength(1);
+            int[,] resultMatrix = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    resultMatrix[i, j] = sumFunction(matrix1[i, j], matrix2[i, j]);
+                }
+            }
+            return resultMatrix;
+        }
+    }
+}
---
+++ LR3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LR3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = GetRandomMatrix(3, 3);
+            int[,] matrix2 = GetRandomMatrix(3, 3);
+
+            AsyncCallback callback = new AsyncCallback(SumCallback);
+            SumDelegate sumDelegate = new SumDelegate(SumMatrices);
+            IAsyncResult result = sumDelegate.BeginInvoke(matrix1, matrix2, callback, null);
+            Console.WriteLine("Calculating sum of matrices...");
+
+            // Wait for the result
+            while (!result.IsCompleted)
+            {
+                Console.WriteLine("Waiting for result...");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Sum of matrices:");
+            PrintMatrix(sumDelegate.EndInvoke(result));
+            Console.ReadKey();
+        }
+
+        private static int[,] GetRandomMatrix(int rows, int columns)
+        {
+            int[,] matrix = new int[rows, columns];
+            Random random = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = random.Next(10);
+                }
+            }
+            return matrix;
+        }
+
+        private delegate int[,] SumDelegate(int[,] matrix1, int[,] matrix2);
+
+        private static int[,] SumMatrices(int[,] matrix1, int[,] matrix2)
+        {
+            int rows = matrix1.GetLength(0);
+            int columns = matrix1.GetLength(1);
+            int[,] sum = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    sum[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return sum;
+        }
+
+        private static void SumCallback(IAsyncResult result)
+        {
+            Console.WriteLine("Sum completed.");
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
